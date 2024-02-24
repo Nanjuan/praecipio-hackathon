0b48fc1cb669854da98bc062989e7ad96fb4c897
Stephen <durans2@Protonmail.com>
2024-02-24 11:29:30 -0500

    boilerplate code

--- API/ProPublica.py
+++ API/ProPublica.py
@@ -1,2 +1,12 @@
 #Author: Stephen Duran
 #Propublica API usage file
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"]) 
