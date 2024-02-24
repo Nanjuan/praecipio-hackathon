ffe0d4ffbd7ea878a3666ac293752dc3062c7f76
Alex <amiller@konnectagency.com>
2024-02-24 16:13:59 -0500

    merge alex and nestor

--- frontend/src/app/theme.js
+++ frontend/src/app/theme.js
@@ -1,5 +1,12 @@
 "use client";
+import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const theme = createTheme({
   typography: {
